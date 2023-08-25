775973af1cb44630e7ed7994b375afae8f829c26
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-08-24 21:16:45 -0300

    clase31

--- Clase_31/src/app.js
+++ Clase_31/src/app.js
@@ -15,10 +15,117 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", usersRouter);
 
 const SERVER_PORT = config.port;
+
+
 app.listen(SERVER_PORT, () => {
     console.log("Servidor escuchando por el puerto: " + SERVER_PORT);
-
     //Logica a implementar - Test de pruebas
+    const executeTest = config.runTests
+    if (executeTest) {
+        console.log("Ejecutando set de pruebas para suma:");
+        // Escenarios
+        let testPasados = 0
+        const testTotales = 4
 
+
+        // Test 1: La función debe devolver null si algun parametro no es numérico.
+        testPasados = escenario1(testPasados);
+
+
+        //Test 2: La funcion debe devolver 0 si no se pasa ningún parámetro:
+        testPasados = escenario2(testPasados);
+
+
+        //Test 3: La función debe poder realizar la suma correctamente.
+        testPasados = escenario3(testPasados);
+
+
+        //Test 4: La función debe poder realizar la suma con cualquier cantidad de numeros.
+        testPasados = escenario4(testPasados);
+
+
+
+        console.log(`Test ejecutados: ${testTotales}, pasaron: ${testPasados}`);
+    }
 });
 
+
+const escenario1 = (testPasados) => {
+    console.log("Test 1: La función debe devolver null si algun parametro no es numérico.");
+    // Given
+    const num1 = "2"
+    const num2 = 5
+
+    // Then 
+    let result = suma(num1, num2)
+
+
+    // Assert o validacion.
+    if (result === null) {
+        console.log("Test 1: Success!\n");
+        testPasados++;
+    } else console.log(`Test 1: No pasa, se recibio ${typeof result}, pero se esperaba null`);
+    return testPasados;
+}
+
+
+
+const escenario2 = (testPasados) => {
+    console.log("Test 2: La funcion debe devolver 0 si no se pasa ningún parámetro:");
+    // Given
+
+
+    // Then 
+    let result = suma()
+
+
+    // Assert o validacion.
+    if (result === 0) {
+        console.log("Test 2: Success!\n");
+        testPasados++;
+    } else console.log(`Test 2: No pasa, se recibio ${result}, pero se esperaba 0`);
+    return testPasados;
+}
+
+
+
+const escenario3 = (testPasados) => {
+    console.log("Test 3: La función debe poder realizar la suma correctamente.");
+    // Given
+    const num1 = 2
+    const num2 = 5
+
+
+    // Then 
+    let result = suma(num1, num2)
+
+
+    // Assert o validacion.
+    if (result === 7) {
+        console.log("Test 3: Success!\n");
+        testPasados++;
+    } else console.log(`Test 3: No pasa, se recibio ${result}, pero se esperaba ${num1 + num2}`);
+    return testPasados;
+}
+
+
+
+
+const escenario4 = (testPasados) => {
+    console.log("Test 4: La función debe poder realizar la suma con cualquier cantidad de numeros");
+    // Given
+    const numeros = [1, 2, 3, 4, 5]
+
+
+    // Then 
+    let result = suma(...numeros)
+
+
+    // Assert o validacion.
+    const expected = 15
+    if (result === expected) {
+        console.log("Test 4: Success!\n");
+        testPasados++;
+    } else console.log(`Test 4: No pasa, se recibio ${result}, pero se esperaba ${expected}`);
+    return testPasados;
+}
--- Clase_31/src/controllers/users.controller.js
+++ Clase_31/src/controllers/users.controller.js
@@ -3,10 +3,18 @@ import { generateUser } from '../utils.js'
 export const getUsers = async (req, res) => {
     try {
         //Logica a implementar
-
-        res.send({ status: "success", payload: "temporaly out of service -  Contact development team!" })
+        let users = [];
+        let numUsuarios = 200
+        for (let index = 0; index < numUsuarios; index++) {
+            users.push(generateUser())
+        }
+        res.send({ status: "success", payload: users })
     } catch (error) {
         console.error(error);
         res.status(500).send({ error: error, message: "No se pudo obtener los usuarios:" });
     }
 };
+
+
+
+
